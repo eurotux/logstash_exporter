8d7e02e52c61915a30ee9c16104315bf5fa4fb99
Diogo Vieira <dfv@eurotux.com>
2018-08-23 18:03:01 +0100

    Fix missing package from HandleFunc usage

--- logstash_exporter.go
+++ logstash_exporter.go
@@ -54,7 +54,7 @@ func listen(exporterBindAddress string) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/metrics", http.StatusMovedPermanently)
 	})
-	HandleFunc("/_/health", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/_/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`ok`))
 	})
 
